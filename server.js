d9243411af1f3794d3ee7172d1ed3ea4ea6ed9ef
dbehshhhej <82349684+dbehshhhej@users.noreply.github.com>
2024-11-20 15:23:29 -0600

    Beginnings of node

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {res.render('index.ejs')})
+app.listen(port, () => console.log("Listening on port 3000"))
