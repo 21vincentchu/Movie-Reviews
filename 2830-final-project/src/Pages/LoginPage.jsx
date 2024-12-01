40d564602cf4dafed0e5f2fedcb61fcd3c1e09c6
dbehshhhej <82349684+dbehshhhej@users.noreply.github.com>
2024-12-01 17:23:00 -0600

    More functional login, still needs to be connected to db

--- 2830-final-project/src/Pages/LoginPage.jsx
+++ 2830-final-project/src/Pages/LoginPage.jsx
@@ -4,31 +4,43 @@ import '../login-page.css'
 function LoginPage({}){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');   
 
 
-    const handleSubmit = (e) => {
-       e.preventDefault();
-       console.log('Username:', username);
-       console.log('Password:', password);
+    const handleSubmit = ( event ) => {
+       event.preventDefault();
+       if(!username || !password){
+            setErrorMessage('Please enter a Username and Password');
+       }
+       else{
+        console.log('Username:', username);
+        console.log('Password:', password);
+        setErrorMessage('');
+       }
      };
 
     return(
         <div>
-            <h1 className='login-welcome'>Welcome to FreshTomato!</h1>
-            <p className='login-subtext'>Let's get you logged in.</p>
+            <div className='circle-background'></div>
+            <div className='content'>
+                <h1 className='login-welcome'>Welcome to </h1>
+                <h1 className='login-welcome'>FreshTomato!</h1>
+                <p className='login-subtext'>Let's get you logged in.</p>
 
-            <div className='login-form'>
-                <div className='login-field'>
-                    <label className="login-label" htmlFor="un">Username</label>
-                    <input name='un' className='login-entrybox' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </div>
-                <div className='login-field'>
-                    <label className="login-label"  htmlFor="pwd">Password</label>
-                    <input name='pwd' type="text" className='login-entrybox' value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div className='login-field'>
-                    <button type="submit" onClick={handleSubmit}>Submit</button>
-                </div>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <div className='login-field'>
+                        <label className="login-label" htmlFor="un">Username</label>
+                        <input name='un' className='login-entrybox' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className='login-field'>
+                        <label className="login-label"  htmlFor="pwd">Password</label>
+                        <input name='pwd' type="password" className='login-entrybox' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='login-field'>
+                        <button type="submit" className='welcome-submit-button'>Login</button>
+                    </div>
+                </form>
+                {errorMessage && (<div className="error-message">{errorMessage}</div>)}
             </div>
         </div>
     )
